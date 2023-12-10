ea796f50cd99dd71c6e6c95dfb80b99180378c71
Ssserhiyyy <serhii.komaryn.pm.2022@lpnu.ua>
2023-12-10 22:24:16 +0200

    lab2

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,13 @@
- 
+function toggleNav() {
+    var sideNav = document.getElementById("sideNav");
+    var main = document.getElementById("main");
+    var isOpen = sideNav.style.width === "250px";
+
+    if (isOpen) {
+        sideNav.style.width = "0";
+        main.style.marginLeft = "0";
+    } else {
+        sideNav.style.width = "250px";
+        main.style.marginLeft = "250px";
+    }
+}
